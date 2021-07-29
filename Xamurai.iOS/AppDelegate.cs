141a77a215bcd5dfa6d75b9cd5005465258e6be8
Malcolm Jack <inquisitorjax@gmail.com>
2021-07-29 16:22:49 +0200

    MT-53107: Add Challenge #2 (#3)
    * update spec to be more clear
    
    * update
    
    * update xamurai project
    
    * xaml challenge on list view page
    
    * update challenge 2
    
    * update copy
    
    * Update Xamurai/.Docs/Dojo.md
    
    Co-authored-by: MultiTracks <dev@multitracks.com>
    
    * update copy
    
    * rules first
    
    Co-authored-by: MultiTracks <dev@multitracks.com>

--- Xamurai.iOS/AppDelegate.cs
+++ Xamurai.iOS/AppDelegate.cs
@@ -19,7 +19,6 @@ namespace Xamurai.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-			Xamarin.Forms.Forms.SetFlags("Shell_Experimental", "CollectionView_Experimental");
 			global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
 
