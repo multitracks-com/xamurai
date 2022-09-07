da012fae2b7cb7e53fcedbadb1b0322fe714a2bb
tybeck3 <74684665+tybeck3@users.noreply.github.com>
2022-09-06 22:59:31 -0700

    Add files via upload

---
+++ PagedCollectionPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamurai
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PagedCollectionPage : ContentPage
+	{
+		public PagedCollectionPage ()
+		{
+			BindingContext = new SampleViewModel();
+			InitializeComponent ();
+		}
+	}
+}
